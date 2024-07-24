d2f4b3afc4ac668abf238a262bab3e5c9cbb09c4
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-07-24 08:28:41 -0400

    Move "urlsForUser" helper function to "helpers.js" file. Export the "urlsForUsers" function from "helpers.js" using "module.exports". Refactor "urlsForUser" helper function by adding a second parameter to now take in both the urls database (urlDatabase) and the already implemented users id parameter. Rename the users id parameter to from “id” to "userid" for clarity. Ensure everything in the app still works as before by running it.

--- helpers.js
+++ helpers.js
@@ -8,5 +8,16 @@ const findUserByEmail = function(email, userDatabase) {
   return undefined; // Return undefined if no matching user is found
 };
 
+// Function that returns the URLs where the userID is equal to the id of the currently logged-in user
+const urlsForUser = function(userid, urlDatabase) {
+  const userUrls = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === userid) {
+      userUrls[shortURL] = urlDatabase[shortURL]; // If userID matches the id, add the URL entry to the userUrls object
+    }
+  }
+  return userUrls; // Return the filtered userUrls object containing only URLs for the given user ID
+};
+
 // Export helper function to make it available throughout the project
-module.exports = { findUserByEmail };
+module.exports = { findUserByEmail, urlsForUser };
