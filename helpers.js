eef8ec3e5686c71fa03114510b5cf47d240ddc82
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-07-24 05:51:53 -0400

    Create a new file called "helpers.js" in the main project directory to house helper functions. Move "findUserByEmail" helper function to newly created "helpers.js" file. Export the "findUserByEmail" function from "helpers.js" using "module.exports".

---
+++ helpers.js
@@ -0,0 +1,12 @@
+// Helper function to find user by email
+const findUserByEmail = function(email, userDatabase) {
+  for (const user of Object.values(userDatabase)) {
+    if (user.email === email) { // Check if the current user's email matches the provided email
+      return user; // Return the user object if the email matches
+    }
+  }
+  return null; // Return null if no matching user is found
+};
+
+// Export helper function to make it available throughout the project
+module.exports = { findUserByEmail };
